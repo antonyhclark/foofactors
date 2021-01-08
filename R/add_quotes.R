9c724289d4d6f9b005918b19b5c57894574a47c7
Antony Clark <anthoc03@nssrstudio03.csa.scot.nhs.uk>
2021-01-08 16:53:49 +0000

    commit with documentation, licence, testing

--- R/add_quotes.R
+++ R/add_quotes.R
@@ -1,4 +1,15 @@
 
-add_quotes <- function(x) paste0('\"',x,'\"')
-add_quotes("Z")
+#' Put double quote around text
+#'
+#' Enclose text in quotes as may be useful for sending commands to Linux terminal,
+#' e.g. if a path contains spaces or ampersands
+#' @param x character
+#'
+#' @return character
+#' @export
+#'
+#' @examples
+#' add_quotes("~/badly & named directory")
+add_quotes <- function(x) {paste0('\"',x,'\"')}
+
 
---
+++ tests/testthat/test-add_quotes.R
@@ -0,0 +1,3 @@
+test_that("quotes are added", {
+  expect_equal("\"x\"",add_quotes("x"))
+})
